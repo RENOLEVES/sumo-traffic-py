d60f330bc39fdc6028aa54847d55c86b137d2860
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2020-07-06 15:36:55 -0400

    Initial Dump Of Files
    Initial dump of files to main branch.

---
+++ createOutputFiles.bat
@@ -0,0 +1,7 @@
+sumo -c lachine.sumocfg --fcd-output Outputs/traceVehiclePosition.xml
+
+sumo -c lachine.sumocfg --emission-output Outputs/traceEmission.xml
+
+sumo -c lachine.sumocfg --lanechange-output Outputs/traceLaneChange.xml
+
+rem sumo -c lachine.sumocfg --vtk-output Outputs/VTK/trace_vkt.xml
