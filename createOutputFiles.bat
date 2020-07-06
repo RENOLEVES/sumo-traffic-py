9e63b5c6b4bccf39cc7a04934d8ba554259a99bf
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2020-07-06 15:58:15 -0400

    Update createOutputFiles.bat
    Allow the user to cancel

--- createOutputFiles.bat
+++ createOutputFiles.bat
@@ -1,7 +1,11 @@
+set /p cont=The following files may take a while to create. Do you wish to continue? (y/n): 
+
+if %cont% == n exit
+
 sumo -c lachine.sumocfg --fcd-output Outputs/traceVehiclePosition.xml
 
 sumo -c lachine.sumocfg --emission-output Outputs/traceEmission.xml
 
 sumo -c lachine.sumocfg --lanechange-output Outputs/traceLaneChange.xml
 
-rem sumo -c lachine.sumocfg --vtk-output Outputs/VTK/trace_vkt.xml
+sumo -c lachine.sumocfg --vtk-output Outputs/VTK/trace_vkt.xml
