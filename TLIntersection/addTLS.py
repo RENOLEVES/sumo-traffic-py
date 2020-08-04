60b47ebe2067905e3f53e62c207293a8a8fd0f6b
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2020-08-04 15:59:24 -0400

    Create and Update TLS
    Can add and update traffic lights to a net file. Can add and update traffic schedules to a net file.

---
+++ TLIntersection/addTLS.py
@@ -0,0 +1,74 @@
+
+import argparse
+from os import path
+
+
+def fillOptions(argParser):
+    argParser.add_argument("-n", "--sumo-net-file", metavar="FILE",
+                            help="Read SUMO-net from FILE")
+    argParser.add_argument("-c", "--csv-file", metavar="FILE",
+                            help="Read TLS locations from FILE")
+    argParser.add_argument("-o", "--node-output-file", 
+                            metavar="FILE", default='tls.nod.xml',
+                            help="The generated nodes will be written to FILE")
+
+
+def parse_args(args=None):
+    argParser = argparse.ArgumentParser()
+    fillOptions(argParser)
+    return argParser.parse_args(args), argParser
+
+
+if __name__ == "__main__":
+    options, argParser = parse_args()
+
+    # Get net file
+    if not options.sumo_net_file:
+        argParser.print_help()
+        argParser.exit("Error! Providing a net file is mandatory")
+    else:
+        if not path.exists(options.sumo_net_file):
+            argParser.exit("Error! Net file not found")
+
+    # Get csv file
+    if not options.csv_file:
+        argParser.print_help()
+        argParser.exit("Error! Providing a csv file is mandatory")
+    else:
+        if not path.exists(options.csv_file):
+            argParser.exit("Error! CSV file not found")
+
+    import sumolib
+    import pandas as pd
+    from xml.etree import ElementTree as ET
+    from shapely.geometry import polygon
+
+    net = sumolib.net.readNet(options.sumo_net_file)
+    nodes = net.getNodes()
+
+    df = pd.read_csv(options.csv_file)
+    df = df.loc[df['Arrondissement'] == 'Lachine']
+
+    topElem = ET.Element("nodes")
+
+
+    for node in nodes:
+        id = node.getID()
+        shape = node.getShape()
+        if len(shape) < 3:
+            continue
+        
+        if node.getType() == 'traffic_light':
+            elem = ET.SubElement(topElem,'node')
+            elem.attrib = {'id':id, 'type':'traffic_light'}
+        else:
+            for _, row in df.iterrows():
+                lon, lat = row['long'], row['lat']
+                x, y = net.convertLonLat2XY(lon,lat)
+
+                if polygon.Polygon(shape).contains(polygon.Point(x,y)):
+                    elem = ET.SubElement(topElem,'node')
+                    elem.attrib = {'id':id, 'type':'traffic_light'}
+
+    tree = ET.ElementTree(topElem)
+    tree.write(options.node_output_file)
--- TLIntersection/createTLS.bat
+++ TLIntersection/createTLS.bat
@@ -1,4 +1,21 @@
+@ECHO OFF
+set /p net="Enter file path of the SUMO network file. >"
+set /p csv="Enter file path of the intersection csv file. >"
+set /p output="Enter file path of the output SUMO network file. >"
+set /p useSched="Do you wish to use a file containing schedules for the intersections? (y/n) >"
 
-python addTLSToNodes.py -n "../lachine.net.xml" -c "intersectionsafeux.csv"
+if %useSched% == y (
+    set /p schedules="Enter path of the schedules for the intersections file. >"
+)
 
-netconvert --sumo-net-file "../lachine.net.xml" --node-files "tls.nod.xml" -o "../lachine.net.xml"
+@ECHO ON
+
+python addTLS.py -n %net% -c %csv% -o "tls.nod.xml"
+
+netconvert --sumo-net-file %net% --node-files "tls.nod.xml" -o %output% --tls.allred.time 3 --tls.cycle.time 100 --tls.discard-loaded
+
+if %useSched% != y exit
+
+python addTLSPrograms.py -n %net% -o "tlsProgram.add.xml" -c %schedules%
+
+netconvert -s %net% -i "tlsProgram.add.xml" -o %output%
