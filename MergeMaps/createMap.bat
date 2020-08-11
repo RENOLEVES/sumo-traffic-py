e23a29376b88cecd30ab5e3f3fa658ea2ddcd71b
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2020-08-11 17:02:22 -0400

    Choose Input Files
    Can choose input files from text now

--- MergeMaps/createMap.bat
+++ MergeMaps/createMap.bat
@@ -1,6 +1,9 @@
-rem To specify files, change file paths that are in quotations to deswired files.
 
-python createPoly.py -g "montreal_boundary.geojson" -p poly-file.poly
-osmconvert "large-osm.osm.pbf" -B=poly-file.poly -o=osm-file.osm
+set /p boundries=Enter the boundries file: 
+set /p largeOSM=Enter the large OSM file containg the boundry: 
+set /p outputFile=Enter the SUMO net file: 
+
+python createPoly.py -g %boundries% -p poly-file.poly
+osmconvert %largeOSM% -B=poly-file.poly -o=osm-file.osm
 python addOSM.py -osm osm-file.osm
-netconvert --osm-files osm-file.osm -o "montreal-net.net.xml" --output.street-names=True --output.original-names=True --tls.guess-signals=True --tls.default-type="actuated" --crossings.guess=True
+netconvert --osm-files osm-file.osm -o %outputFile% --output.street-names=True --output.original-names=True --tls.guess-signals=True --tls.default-type="actuated" --crossings.guess=True
