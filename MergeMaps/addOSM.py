9a50e37673881c426d7180d5283bfaf3e863e09c
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2020-08-19 14:22:30 -0400

    Update Random Trip Generation
    Update random trip generation for montreal map

--- MergeMaps/addOSM.py
+++ MergeMaps/addOSM.py
@@ -15,5 +15,17 @@ def parse_args(args=None):
 if __name__ == "__main__":
     options, argParser = parse_args()
 
-    with open(options.osm_file, "a") as f:
-        f.write("</osm>")
+    osm = True
+    with open(options.osm_file, "r") as f:
+        line = f.readline()
+        while line:
+            if "</osm>" in line:
+                osm = False
+                break
+            try:
+                line = f.readline()
+            except UnicodeDecodeError:
+                pass
+    if osm:    
+        with open(options.osm_file, "a") as f:
+            f.write("</osm>")
--- MergeMaps/randomTrip.bat
+++ MergeMaps/randomTrip.bat
@@ -5,12 +5,12 @@ rem -p: the frequency of trips in a second. ex -p 0.5 => a trip is added every 0
 rem -b: the beginning time that the first trip arrives
 rem -e: the end time that the last trip arrives
 rem --seed: an initial seed for psuedo randomness
-rem --fringe-factor: the probability that a trip starts at an edge with no successor or predecessor
+rem --fringe-factor: the probability that a trip starts or ends at an edge with no successor or predecessor
 rem Total number of vehicles is: (e - b) / p
 
 @ECHO ON
 
-python "%SUMO_HOME%tools\randomTrips.py" -n montreal.net.xml --seed 42 --fringe-factor 2 -p 1.5 -e 600 -o Trips/montreal.motorcycle.trips.xml --vehicle-class motorcycle --vclass motorcycle --prefix moto --fringe-start-attributes "departSpeed=\"max\"" --max-distance 1200 --trip-attributes "departLane=\"best\"" --validate
+python "%SUMO_HOME%tools\randomTrips.py" -n montreal.net.xml --seed 42 --fringe-factor 2 -p 1.5 -e 600 -o Trips/montreal.motorcycle.trips.xml --vehicle-class motorcycle --vclass motorcycle --prefix moto --fringe-start-attributes "departSpeed=\"max\"" --min-distance 400 --trip-attributes "departLane=\"best\"" --validate
 python "%SUMO_HOME%tools\randomTrips.py" -n montreal.net.xml --seed 42 --fringe-factor 5 -p 0.75 -e 600 -o Trips/montreal.passenger.trips.xml --vehicle-class passenger --vclass passenger --prefix veh --min-distance 300 --trip-attributes "departLane=\"best\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --lanes --validate
 python "%SUMO_HOME%tools\randomTrips.py" -n montreal.net.xml --seed 42 --fringe-factor 5 -p 2.5 -e 600 -o Trips/montreal.truck.trips.xml --vehicle-class truck --vclass truck --prefix truck --min-distance 600 --fringe-start-attributes "departSpeed=\"max\"" --trip-attributes "departLane=\"best\"" --validate
 python "%SUMO_HOME%tools\randomTrips.py" -n montreal.net.xml --seed 42 --fringe-factor 5 -p 2 -e 600 -o Trips/montreal.bus.trips.xml --vehicle-class bus --vclass bus --prefix bus --min-distance 600 --fringe-start-attributes "departSpeed=\"max\"" --trip-attributes "departLane=\"best\"" --validate
