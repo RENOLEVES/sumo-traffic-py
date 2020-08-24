a1b944171db4e056233c9d7fb4f78bc6e6dbaef1
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2020-08-24 17:02:11 -0400

    Reorganize Folders

--- MergeMaps/createMap.bat
+++
@@ -1,9 +0,0 @@
-
-set /p boundries=Enter the boundries file: 
-set /p largeOSM=Enter the large OSM file containg the boundry: 
-set /p outputFile=Enter the SUMO net file: 
-
-python createPoly.py -g %boundries% -p poly-file.poly
-osmconvert %largeOSM% -B=poly-file.poly -o=osm-file.osm
-python addOSM.py -osm osm-file.osm
-netconvert --osm-files osm-file.osm -o %outputFile% --output.street-names=True --output.original-names=True --tls.guess-signals=True --tls.default-type="actuated" --crossings.guess=True
--- README.md
+++ README.md
@@ -11,12 +11,16 @@ Components
  - Creating Large Netfiles
 
 SUMO github page:
+
         https://github.com/eclipse/sumo
 
 SUMOlib and TraCI are two python modules to communicate with the SUMO application.
 SUMOlib pydocs page:
+
         https://sumo.dlr.de/pydoc/sumolib.html
+
 TraCI pydocs page:
+
         https://sumo.dlr.de/pydoc/traci.html
 
 
