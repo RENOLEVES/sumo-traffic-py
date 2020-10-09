258382a1a2a1c10cdaa6378210ff80b703a38863
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2020-10-09 10:59:38 -0400

    Fixed No Stops After Rerouting Error
    Once a vehicel is done charging, new route with stops is added.

--- sumoplustools/chargingstations/routeToCharge.py
+++ sumoplustools/chargingstations/routeToCharge.py
@@ -8,10 +8,16 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 from sumoplustools.stopsigns import addStops 
 
 class RerouteChargingDomain():
-    def __init__(self, sumocfgFile, connection : traci.Connection):
+    def __init__(self, sumocfgFile, connection : traci.Connection, netFile=None, addFiles=None):
         self.sumocfgFile = sumocfgFile
-        self.net = self.getNetworkFile()
-        self.additionalFiles = self.getAdditionalFiles()
+        if netFile:
+            self.net = netFile
+        else:
+            self.net = self.getNetworkFile()
+        if addFiles:
+            self.additionalFiles = addFiles
+        else:
+            self.additionalFiles = self.getAdditionalFiles()
         self.chargingElems = self.getChargingStationElems()
         self.connection = connection
         self._vehiclesRerouted = {}
@@ -54,12 +60,43 @@ class RerouteChargingDomain():
         return chargingStationElems
 
 
-    def getChargingStationIDs(self):
+    def getChargingStationIDs(self) -> list(str):
         return [elem.get("id") for elem in self.chargingElems]
 
-    def getChargingStationLanes(self):
+    def getChargingStationLanes(self) -> [(str, str)]:
         return [(elem.get("id"), elem.get("lane")) for elem in self.chargingElems]
     
+    def getOccupiedChargingStations(self) -> list(str):
+        chargingStations = []
+        for vehicle in list(self._vehiclesRerouted.values()):
+            chargingStations += [vehicle['chargingStation']]
+        return chargingStations
+
+    def getClosestChargingStation(self, vehID) -> (str,str):
+        """
+        Returns the closest charging station and its edge in a pair (chargingStationID, edgeID)
+        """
+        shortestTime = np.inf
+        closestChargingStation = closestEdge = None
+        # Loop for each charging station in the network
+        for chargingStationID, chargingStationLaneID in self.getChargingStationLanes():
+            # Check if another vehicle is on route to or charging at the charging station
+            if chargingStationID in self.getOccupiedChargingStations():
+                # Go to next charging station
+                continue
+
+            chargeEdgeID = self.connection.lane.getEdgeID(chargingStationLaneID)
+            try:
+                travelTime = self.connection.vehicle.getParameter(vehID,"device.rerouting.edge:%s" % chargeEdgeID)
+            except:
+                travelTime = self.connection.simulation.getDistanceRoad(edgeID1=self.connection.vehicle.getRoadID(vehID), pos1=0, edgeID2=chargeEdgeID, pos2=0, isDriving=True)
+            # Get closest charging station by travel time
+            if float(travelTime) < shortestTime:
+                shortestTime = float(travelTime)
+                closestChargingStation = chargingStationID
+                closestEdge = chargeEdgeID
+        return closestChargingStation, closestEdge
+        
     def addStopsToVehicle(self, vehID):
         startPos, endPos, duration  = addStops.getStopParam()
         routeID = self.connection.vehicle.getRouteID(vehID)
@@ -75,9 +112,11 @@ class RerouteChargingDomain():
         startRerouteThreshold = 20 / 100
         finishRerouteThreshold = finishRerouteThreshold / 100
         generator = np.random.default_rng()
+
+        # Loop for each vehicle in the simulation
         for vehicleID in self.connection.vehicle.getIDList():
-            hasBattery = (self.connection.vehicle.getParameter(vehicleID, "has.battery.device") == "true")
-            if not hasBattery:
+            # Check if the vehicle has a bettery to charge, otherwise continue to next vehicle
+            if not self.connection.vehicle.getParameter(vehicleID, "has.battery.device") == "true":
                 continue
 
             charging = self.connection.vehicle.getParameter(vehicleID, "device.battery.chargingStationId")
@@ -88,20 +127,7 @@ class RerouteChargingDomain():
 
             # Check if vehicle is low on battery, and not at or going to charging station 
             if (float(battery) <= float(batteryTotal) * startRerouteThreshold) and (charging == "NULL") and (vehicleID not in self._vehiclesRerouted):
-                shortestTime = np.inf
-                closestChargingStation = None
-                # Loop for each charging station in the network
-                for (chargingStationID, chargingStationLaneID) in self.getChargingStationLanes():
-                    # Check if another vehicle is on route to the charging station
-                    if chargingStationID in self._vehiclesRerouted[vehicleID].values:
-                        continue
-                    chargeEdgeID = self.connection.lane.getEdgeID(chargingStationLaneID)
-                    travelTime = self.connection.vehicle.getParameter(vehicleID,"device.rerouting.edge:%s" % chargeEdgeID)
-                    # Get closest charging station by travel time
-                    if float(travelTime) < shortestTime:
-                        shortestTime = float(travelTime)
-                        closestChargingStation = chargingStationID
-                        closestEdge = chargeEdgeID
+                closestChargingStation, closestEdge = self.getClosestChargingStation(vehicleID)
                 # If no charging stations available, do not reroute to charge
                 if closestChargingStation is None:
                     continue
