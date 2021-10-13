02722c9feffd6968466ecbc7fca9e24042a6900f
home-imac <ma3725@nyu.edu>
2021-10-13 18:30:03 -0400

    select columns from DB

--- app/api/api_vol/server/flask_server/app.py
+++ app/api/api_vol/server/flask_server/app.py
@@ -1,4 +1,6 @@
-from db_connection import DBConn 
+from flask_socketio import SocketIO, emit
+from flask import Flask, render_template
+from db_connection import DBConn
 from db_query import AgentQuery
 
 # create a database connection instance
@@ -10,39 +12,15 @@ Agents = Conn.get_db_table("agents")
 
 # create a db query instance
 agentQuery = AgentQuery(session=session, model=Agents)
-res = agentQuery.query_floating_time_range(value=10, bufferLength=115)
-print( res )
+res = agentQuery.query_emission_time_range(time_value=10, buffer_length=115, emission_ids=['vts', 'vlon','vname'])
+# from pprint import pprint
+# pprint(res)
+# for el in res:
+#     pprint(el)
+    # pprint(el.vts)
 
 
-
-
-
-# def get_timestamp_all(vts):
-#     # result = session.execute(select(Agents).where(Agents.vts == vts))
-#     # # print( "result:   ", result.scalars() )
-#     # for agent_obj in result.scalars():
-#     #     print(f"{agent_obj.vname} {agent_obj.vts}")
-#     records = session.query(Agents).filter(Agents).first()
-#         # .(vname == 'veh0')
-#         # .all()
-        
-        
-#     for record in records:
-#         print("----------------")
-#         print(record)
-        
-#     # vts = message
-#     # # Retrieve the last number inserted inside the 'numbers'
-#     # query = "" + \
-#     #         "SELECT vname, vts " + \
-#     #         "FROM agents " + \
-#     #         f"WHERE vts = {vts} " + \
-#     #         "ORDER BY " + "vname" + " ASC"
-#     # result_set = db.execute(query).mappings().all()
-#     # print([res for res in result_set])
-
-#     # return [dict(res) for res in result_set] if isinstance(result_set, list) else dict(result_set)
-    
+# pprint(dir(Agents.classes))
 
 def get_emis(message):
     print(f"message: {message}")
@@ -51,51 +29,50 @@ def get_emis(message):
     vts = message['vts']
     query = "" + \
             f"SELECT vemis_{emis_name} " + \
-             "FROM agents " + \
+        "FROM agents " + \
             f"WHERE vname = '{vname}' " + \
             f"AND vts <= {vts} " + \
-             "ORDER BY " + "vname" + " ASC"
+        "ORDER BY " + "vname" + " ASC"
     print(query)
     result_set = db.execute(query).mappings().all()
     return [res.vemis_co2 for res in result_set] if isinstance(result_set, list) else dict(result_set)
-    
+
 
 # from pprint import pprint
 # last = get_emis_co2("veh2", vts=10000)
 # print(last)
 
 
-
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-    
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins='*')
+
+FLOATING_FIELDS = {'lon': 'vlon', 'lat': 'vlat', 'name': 'vname'}
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/home/')
 def home():
     return "Home page"
 
-# @socketio.on('my event')
-# def test_message(message):
-#     emit('my response', {'data1': message})
 
 @socketio.on('float_req')
 def handle_ts(value):
-    value, bufferLength = value
-    last = agentQuery.query_floating_time_range(value=value, bufferLength=bufferLength)
+    value, buffer_length = value
+    last = agentQuery.query_floating_time_range(time_value=value, buffer_length=buffer_length)
     emit('float_res', last)
 
+
 @socketio.on('emis_req')
-def handle_emis(message):
-    last = get_emis(message)
+def handle_emis(value):
+    time_value, buffer_length, emission_ids = value
+    last = agentQuery.query_emission_time_range(time_value=time_value, buffer_length=buffer_length, emission_ids=emission_ids)
     emit('emis_res', last)
+
 
 @socketio.on('connect')
 def test_connect(sid):
@@ -104,13 +81,14 @@ def test_connect(sid):
     print(50*'1')
     emit('connect', 'SOCKET CONNECTED')
 
+
 @socketio.on('disconnect')
 def test_disconnect():
     print(50*'2')
     print('Client DISCONNECTED')
     print(50*'3')
     emit('disconnect', 'SOCKET DISCONNECTED')
-    
+
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=8000)
--- app/api/api_vol/server/flask_server/db_query.py
+++ app/api/api_vol/server/flask_server/db_query.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import load_only
 
 
 class DBQuery():
@@ -16,13 +17,31 @@ class AgentQuery(DBQuery):
     def __init__(self, session=None, model=None):
         super().__init__(session=session, model=model)
         
-    def query_floating_time_range(self, value, bufferLength):
+    def query_floating_time_range(self, time_value, buffer_length):
         model = self.model
-        queried_data = self.session.query(model).filter(model.vts < value+bufferLength).filter(model.vts >= value)
+        queried_data = self.session.query(model).filter(model.vts < time_value+buffer_length).filter(model.vts >= time_value)
         res = {}
         for el in queried_data:
             pack = {'lon':el.vlon, 'lat':el.vlat, 'name':el.vname }
-            # pack = [el.vname, el.vlon, el.vlat]
+            if el.vts in res:
+                res[el.vts].append(pack)
+            else:
+                res[el.vts] = [pack]
+        return res
+    
+    def query_emission_time_range(self, time_value, buffer_length, emission_ids = []):
+        model = self.model
+        queried_data = (self.session
+                            .query(model)
+                            .filter(model.vts < time_value+buffer_length)
+                            .filter(model.vts >= time_value)
+                            .with_entities(*[model.__dict__[col] for col in emission_ids]) # filter by columns in emission_ids list
+                        )
+
+        res = {}
+    
+        for el in queried_data:
+            pack = {i:el.__getattribute__(i) for i in emission_ids}
             if el.vts in res:
                 res[el.vts].append(pack)
             else:
